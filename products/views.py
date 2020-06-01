94e41bac0f5f7181fd3e8cd516eab3e9f451c36d
marklewis27 <mark.lewis87@googlemail.com>
2020-06-01 17:03:09 +0000

    Added in the prodcuts views on the main page

--- products/views.py
+++ products/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from .models import Product
 
 # Create your views here.
+
+
+def all_products(request):
+    """ A view to show all of the products on the site """
+
+    products = Product.objects.all()
+
+    context = {
+        'products': products,
+    }
+
+    return render(request, 'products/products.html', context)
