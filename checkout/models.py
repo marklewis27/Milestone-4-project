abede80843afa94821c61e44a482c7676c72b15c
marklewis27 <mark.lewis87@googlemail.com>
2020-06-09 16:10:22 +0000

    Added in the form, admin and signals to the checkout app

--- checkout/models.py
+++ checkout/models.py
@@ -16,7 +16,6 @@ class Order(models.Model):
     postcode = models.CharField(max_length=20, null=True, blank=True)
     town_or_city = models.CharField(max_length=40, null=False, blank=False)
     street_address1 = models.CharField(max_length=80, null=False, blank=False)
-    street_address1 = models.CharField(max_length=80, null=True, blank=True)
     county = models.CharField(max_length=80, null=True, blank=True)
     date = models.DateTimeField(auto_now_add=True)
     delivery_cost = models.DecimalField(max_digits=6, decimal_places=2, null=False, default=0)
