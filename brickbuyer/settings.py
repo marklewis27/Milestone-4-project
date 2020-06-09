bc293e7bd4e0886d211402805e16d157678b61e9
marklewis27 <mark.lewis87@googlemail.com>
2020-06-09 16:35:40 +0000

    Added in crispy forms, setup checkout css file and the template

--- brickbuyer/settings.py
+++ brickbuyer/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'products',
     'bag',
     'checkout',
+    'crispy_forms'
 ]
 
 MIDDLEWARE = [
@@ -58,6 +59,8 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'brickbuyer.urls'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 TEMPLATES = [
     {
@@ -75,6 +78,10 @@ TEMPLATES = [
                 'django.contrib.messages.context_processors.messages',
                 'bag.contexts.bag_contents',
             ],
+            'builtins': [
+                'crispy_forms.templatetags.crispy_form_tags',
+                'crispy_forms.templatetags.crispy_form_field',
+            ]
         },
     },
 ]
