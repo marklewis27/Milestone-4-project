40d61914848f5c1ab9b1d5071e404e670f97d82d
marklewis27 <mark.lewis87@googlemail.com>
2020-06-10 17:00:19 +0000

    Trying to find bug in the payment

--- checkout/signals.py
+++ checkout/signals.py
@@ -6,12 +6,15 @@ from .models import OrderLineItem
 
 @receiver(post_save, sender=OrderLineItem)
 def update_on_save(sender, instance, created, **kwargs):
-    """ Update the order total in the update / create """
+    """
+    Update order total on lineitem update/create
+    """
     instance.order.update_total()
 
 
 @receiver(post_delete, sender=OrderLineItem)
 def update_on_delete(sender, instance, **kwargs):
-    """ Update the order total in the update / create """
+    """
+    Update order total on lineitem delete
+    """
     instance.order.update_total()
-
