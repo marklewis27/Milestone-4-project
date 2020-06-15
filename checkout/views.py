d7ab2c526842000dcbc457f73325c9b04a7c397f
marklewis27 <mark.lewis87@googlemail.com>
2020-06-15 18:25:46 +0000

    Added in the final bits for the webhooks

--- checkout/views.py
+++ checkout/views.py
@@ -25,7 +25,7 @@ def cache_checkout_data(request):
         return HttpResponse(status=200)
     except Exception as e:
         messages.error(request, 'Sorry, your payment cannot be \
-            processed at this time.')
+            processed right now. Please try again later.')
         return HttpResponse(content=e, status=400)
 
 
@@ -44,11 +44,16 @@ def checkout(request):
             'postcode': request.POST['postcode'],
             'town_or_city': request.POST['town_or_city'],
             'street_address1': request.POST['street_address1'],
+            'street_address2': request.POST['street_address2'],
             'county': request.POST['county'],
         }
         order_form = OrderForm(form_data)
         if order_form.is_valid():
-            order = order_form.save()
+            order = order_form.save(commit=False)
+            pid = request.POST.get('client_secret').split('_secret')[0]
+            order.stripe_pid = pid
+            order.original_bag = json.dumps(bag)
+            order.save()
             for item_id, item_data in bag.items():
                 try:
                     product = Product.objects.get(id=item_id)
