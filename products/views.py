65e840e098a6ebd1234abd4754dbaa9926161ffd
marklewis27 <mark.lewis87@googlemail.com>
2020-06-18 16:42:12 +0000

    Added the edit and delete buttons to the all products page

--- products/views.py
+++ products/views.py
@@ -71,13 +71,13 @@ def product_detail(request, product_id):
 
 
 def add_product(request):
-    """ Add a new product """
+    """ Add a product to the store """
     if request.method == 'POST':
         form = ProductForm(request.POST, request.FILES)
         if form.is_valid():
-            form.save()
+            product = form.save()
             messages.success(request, 'Successfully added product!')
-            return redirect(reverse('add_product'))
+            return redirect(reverse('product_detail', args=[product.id]))
         else:
             messages.error(request, 'Failed to add product. Please ensure the form is valid.')
     else:
@@ -87,6 +87,8 @@ def add_product(request):
     context = {
         'form': form,
     }
+
+    return render(request, template, context)
 
 
 def edit_product(request, product_id):
@@ -109,4 +111,13 @@ def edit_product(request, product_id):
         'form': form,
         'product': product,
     }
+
     return render(request, template, context)
+
+
+def delete_product(request, product_id):
+    """ Delete a product from the store """
+    product = get_object_or_404(Product, pk=product_id)
+    product.delete()
+    messages.success(request, 'Product deleted!')
+    return redirect(reverse('products'))
